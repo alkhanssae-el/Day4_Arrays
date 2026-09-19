42ab491952744000714fe6bf39c265c23871acf4
alkhanssae-el <alkhanssae.elorche@gmail.com>
2026-09-19 17:11:26 +0100

    save

--- ex7.js
+++ ex7.js
@@ -1,6 +1,6 @@
 //In JavaScript, arrays are variables that have multiple items inside of them, its like a box that has multiple toys in it
 //const prevents the reassignment of a variable, making it constant instead
-const arr = [4, 5]
+const arr = [1, 2]
 arr.push(3)
 console.log(arr)
 //Hence why this works, we didn't reassign the constant but instead only added an item
