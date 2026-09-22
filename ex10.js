29e947519c6e476b16628d2817d9a39022ebb9ab
alkhanssae-el <alkhanssae.elorche@gmail.com>
2026-09-22 15:05:50 +0000

    saves

--- ex10.js
+++ ex10.js
@@ -1,1 +1,11 @@
 let arr = [10, 20, 30, 40]
+let arr1 = []
+
+for (let i = 0; i < arr.length ; i++){
+   let value = 30
+    if (value === arr[i])
+   continue
+  arr1.push(arr[i])
+}
+
+console.log(arr1)
